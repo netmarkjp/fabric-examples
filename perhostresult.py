92f02989e4fa25434a2c10e9c8a293a108e66029
Toshiaki Baba <toshiaki@netmark.jp>
2015-07-02 12:44:23 +0900

    デコレータのほうがきれい

--- perhostresult.py
+++ perhostresult.py
@@ -3,29 +3,28 @@
 from fabric.api import run
 from fabric.api import env
 from fabric.api import execute
+from fabric.decorators import parallel
+from fabric.decorators import runs_once
 
+env.hosts = ["192.168.56.101", "192.168.56.102", "192.168.56.103"]
 env.user = "user0"
 env.password = "password0"
 env.command_timeout = 3600
 env.keepalive = 60
 env.skip_bad_hosts = True
 env.timeout = 3
-env.parallel = True
 
 
+@runs_once
 def go_fab():
-    result = execute(
-        _go_fab, hosts=["192.168.56.101", "192.168.56.102", "192.168.56.103"])
-    execute(print_result, result, hosts=[])
+    result = execute(_go_fab)
+    for k, v in sorted(result.iteritems()):
+        print "result:", k, v
 
 
+@parallel
 def _go_fab():
     result = []
     result.append(run("hostname"))
     result.append(run("whoami"))
     return result
-
-
-def print_result(results):
-    for k, v in sorted(results.iteritems()):
-        print "result:", k, v
